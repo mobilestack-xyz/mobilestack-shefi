3167fbdb27dbde8ca8aa4a450378f7813dd50c67
Kathy Luo <kathyluo18@gmail.com>
2024-09-19 12:14:08 +0200

    feat: add home screen contents

--- src/home/TabHome.tsx
+++ src/home/TabHome.tsx
@@ -1,86 +1,47 @@
-import { NativeStackScreenProps } from '@react-navigation/native-stack'
-import _ from 'lodash'
-import React, { useEffect } from 'react'
-import { useTranslation } from 'react-i18next'
-import { StyleSheet } from 'react-native'
+import React from 'react'
+import { StyleSheet, View } from 'react-native'
+import { ScrollView } from 'react-native-gesture-handler'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import { showMessage } from 'src/alert/actions'
-import { AppState } from 'src/app/actions'
-import { appStateSelector, phoneNumberVerifiedSelector } from 'src/app/selectors'
-import { ALERT_BANNER_DURATION, DEFAULT_TESTNET, SHOW_TESTNET_BANNER } from 'src/config'
-import { refreshAllBalances, visitHome } from 'src/home/actions'
-import { importContacts } from 'src/identity/actions'
-import { Screens } from 'src/navigator/Screens'
-import { StackParamList } from 'src/navigator/types'
-import { phoneRecipientCacheSelector } from 'src/recipients/reducer'
-import { useDispatch, useSelector } from 'src/redux/hooks'
-import { initializeSentryUserContext } from 'src/sentry/actions'
-import { hasGrantedContactsPermission } from 'src/utils/contacts'
+import { DappFeaturedActions } from 'src/dappsExplorer/DappFeaturedActions'
+import DiscoverDappsCard from 'src/dappsExplorer/DiscoverDappsCard'
+import { EarnCardDiscover } from 'src/earn/EarnCard'
+import PointsDiscoverCard from 'src/points/PointsDiscoverCard'
+import { Colors } from 'src/styles/colors'
+import { typeScale } from 'src/styles/fonts'
+import { Spacing } from 'src/styles/styles'
+import networkConfig from 'src/web3/networkConfig'
 
-type Props = NativeStackScreenProps<StackParamList, Screens.TabHome>
-
-function TabHome(_props: Props) {
-  const { t } = useTranslation()
-
-  const appState = useSelector(appStateSelector)
-  const recipientCache = useSelector(phoneRecipientCacheSelector)
-  const isNumberVerified = useSelector(phoneNumberVerifiedSelector)
-
-  const dispatch = useDispatch()
-
-  useEffect(() => {
-    dispatch(visitHome())
-  }, [])
-
-  const showTestnetBanner = () => {
-    dispatch(
-      showMessage(
-        t('testnetAlert.1', { testnet: _.startCase(DEFAULT_TESTNET) }),
-        ALERT_BANNER_DURATION,
-        null,
-        null,
-        t('testnetAlert.0', { testnet: _.startCase(DEFAULT_TESTNET) })
-      )
-    )
-  }
-
-  const tryImportContacts = async () => {
-    // Skip if contacts have already been imported or the user hasn't verified their phone number.
-    if (Object.keys(recipientCache).length || !isNumberVerified) {
-      return
-    }
-
-    const contactPermissionStatusGranted = await hasGrantedContactsPermission()
-    if (contactPermissionStatusGranted) {
-      dispatch(importContacts())
-    }
-  }
-
-  useEffect(() => {
-    // TODO find a better home for this, its unrelated to wallet home
-    dispatch(initializeSentryUserContext())
-    if (SHOW_TESTNET_BANNER) {
-      showTestnetBanner()
-    }
-
-    // Waiting 1/2 sec before triggering to allow
-    // rest of feed to load unencumbered
-    setTimeout(tryImportContacts, 500)
-  }, [])
-
-  useEffect(() => {
-    if (appState === AppState.Active) {
-      dispatch(refreshAllBalances())
-    }
-  }, [appState])
-
-  return <SafeAreaView testID="WalletHome" style={styles.container} edges={[]}></SafeAreaView>
+function TabHome() {
+  return (
+    <ScrollView testID="DiscoverScrollView" scrollEventThrottle={16}>
+      <SafeAreaView testID="DAppsExplorerScreen" style={styles.safeAreaContainer} edges={[]}>
+        <View style={styles.contentContainer}>
+          <DappFeaturedActions />
+          <PointsDiscoverCard />
+          <EarnCardDiscover
+            poolTokenId={networkConfig.aaveArbUsdcTokenId}
+            depositTokenId={networkConfig.arbUsdcTokenId}
+          />
+          <DiscoverDappsCard />
+        </View>
+      </SafeAreaView>
+    </ScrollView>
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
+  safeAreaContainer: {
     flex: 1,
-    position: 'relative',
+    paddingTop: Spacing.Thick24,
+  },
+  contentContainer: {
+    paddingHorizontal: Spacing.Thick24,
+    paddingBottom: Spacing.Thick24,
+  },
+  title: {
+    ...typeScale.titleMedium,
+    color: Colors.black,
+    paddingVertical: Spacing.Thick24,
   },
 })
 
