9b0c9edac79481d83ea78ea2171a3c9a4c6e3c14
Joe Bergeron <jbergero@alum.mit.edu>
2024-11-05 13:41:01 -0500

    fix(restore): Fix CAB restore flow (#16)
    ### Description
    
    See Slack thread
    [here](https://valora-app.slack.com/archives/C07KBTP7RC2/p1730734326274629).
    
    ### Test plan
    
    <!-- Demonstrate the change is solid, or why it doesn't need testing.
    Example: add any manual testing steps or scenarios (if not obvious),
    screenshots / videos if the pull request changes the user interface.
    -->
    
    ### Related issues
    
    - Fixes #[issue number here]
    
    ### Backwards compatibility
    
    <!-- Brief explanation of why these changes are/are not backwards
    compatible. -->
    
    ### Network scalability
    
    If a new NetworkId and/or Network are added in the future, the changes
    in this PR will:
    
    - [ ] Continue to work without code changes, OR trigger a compilation
    error (guaranteeing we find it when a new network is added)

--- src/keylessBackup/SignInWithEmail.tsx
+++ src/keylessBackup/SignInWithEmail.tsx
@@ -177,10 +177,10 @@ function SignInWithEmail({ route }: Props) {
     }
   }
 
-  if (!address) {
+  if (!address && isSetupInOnboarding) {
     return (
       <SafeAreaView style={styles.container}>
-        <View style={styles.activityIndicatorContainer}>
+        <View style={styles.activityIndicatorContainer} testID="SignInWithEmail/Spinner">
           <ActivityIndicator testID="loadingTransferStatus" size="large" color={colors.primary} />
         </View>
       </SafeAreaView>
